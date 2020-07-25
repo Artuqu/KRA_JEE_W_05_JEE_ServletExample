00e6c77e36bfd69cf01ac761d86b95787521b683
Rafal <rafal.opial@coderslab.pl>
2020-07-25 14:29:38 +0200

    Day3. Cookies. Zad1

---
+++ src/main/java/pl/coderslab/web/cookie/Cookie1Get.java
@@ -0,0 +1,36 @@
+package pl.coderslab.web.cookie;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//Zadanie 1 - rozwiązywane z wykładowcą
+//        W projekcie stwórz trzy servlety Cookie1Set, Cookie1Get, Cookie1Del.
+//        Servlet Cookie1Set ma być dostępny pod adresem /setCookie.
+//        Ma nastawiać ciasteczko o nazwie User na CodersLab, z ważnością 24h.
+//        Servlet Cookie1Get ma być dostępny pod adresem /showCookie.
+//        Ma wyświetlać zawartość ciasteczka User.
+//        Servlet Cookie1Del ma być dostępny pod adresem /deleteCookie ma kasować ciasteczko o nazwie User.
+//        Jeżeli nie ma takiego ciasteczka, to powinna się wyświetlić napis: BRAK.
+
+@WebServlet(name = "Cookie1Get", urlPatterns = {"/showCookie"})
+public class Cookie1Get extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Cookie[] cookies = req.getCookies();
+
+        if(cookies != null) {
+            for (Cookie c : cookies) {
+                if ("User".equals(c.getName())) {
+                    resp.getWriter().println(c.getName() + " : " + c.getValue());
+                    break;
+                }
+            }
+
+        }
+    }
+}
---
+++ src/main/java/pl/coderslab/web/cookie/Cookie1Set.java
@@ -0,0 +1,32 @@
+package pl.coderslab.web.cookie;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+//Zadanie 1 - rozwiązywane z wykładowcą
+//        W projekcie stwórz trzy servlety Cookie1Set, Cookie1Get, Cookie1Del.
+//        Servlet Cookie1Set ma być dostępny pod adresem /setCookie.
+//        Ma nastawiać ciasteczko o nazwie User na CodersLab, z ważnością 24h.
+//        Servlet Cookie1Get ma być dostępny pod adresem /showCookie.
+//        Ma wyświetlać zawartość ciasteczka User.
+//        Servlet Cookie1Del ma być dostępny pod adresem /deleteCookie ma kasować ciasteczko o nazwie User.
+//        Jeżeli nie ma takiego ciasteczka, to powinna się wyświetlić napis: BRAK.
+
+@WebServlet(name = "Cookie1Set", urlPatterns = {"/setCookie"})
+public class Cookie1Set extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Cookie cookie = new Cookie("User", "CodersLab");
+        cookie.setMaxAge(24*60*60);
+        cookie.setPath("/");
+
+        resp.addCookie(cookie);
+
+        resp.getWriter().println("Ustawiono ciasteczko");
+    }
+}
